78f11147e194c908769a38659ff7a934416fd5bd
Eric Hoover <ehoover3@yahoo.com>
2025-02-10 21:34:38 -0500

    allow cors for read.php

--- backend/read.php
+++ backend/read.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
