1993b0f1f72a81d1b7208644b2bcea1ad2a6609c
Eric Hoover <ehoover3@yahoo.com>
2025-02-10 20:40:13 -0500

    remove duplicated docker-compose and dockerfile

--- backend/read.php
+++ backend/read.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
@@ -12,27 +12,25 @@ $dbname = $_ENV['MYSQL_DATABASE'];
 $conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo json_encode(['message' => 'Connection failed: ' . $conn->connect_error]);
+    exit;
 }
 
 $sql = "SELECT orderid, comments, shipdate_expected FROM sweetwater_test";
 $result = $conn->query($sql);
 
-$comments = [];
-
 if ($result->num_rows > 0) {
+    $comments = [];
     while ($row = $result->fetch_assoc()) {
         $comments[] = [
-            'orderid' => $row["orderid"],
-            'comments' => $row["comments"],
-            'shipdate_expected' => $row["shipdate_expected"] ? $row["shipdate_expected"] : null
+            'orderid' => $row['orderid'],
+            'comments' => $row['comments'],
+            'shipdate_expected' => $row['shipdate_expected'] ? $row['shipdate_expected'] : null
         ];
     }
+    echo json_encode($comments);
 } else {
-    $comments = ["message" => "No results found."];
+    echo json_encode(['message' => 'No results found.']);
 }
 
 $conn->close();
-
-header('Content-Type: application/json');
-echo json_encode($comments);
