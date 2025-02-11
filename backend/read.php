6b0bcecd47bb360d78b0295179cf19a406dd98cf
Eric Hoover <ehoover3@yahoo.com>
2025-02-10 20:07:59 -0500

    add php file to read the comments

--- backend/read.php
+++ backend/read.php
@@ -1,13 +1,13 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
 $username = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
-$dbname = $_ENV['DB_NAME'];
+$password = $_ENV['MYSQL_ROOT_PASSWORD'];
+$dbname = $_ENV['MYSQL_DATABASE'];
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
